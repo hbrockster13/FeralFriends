43c4c659b30e6d5b37544e6c2be287097cc24c17
hbrockster13 <50593556+hbrockster13@users.noreply.github.com>
2020-01-12 10:24:55 -0600

    Setting up first activity

---
+++ app/src/main/java/com/example/feralfriends/FriendActivity.java
@@ -0,0 +1,30 @@
+package com.example.feralfriends;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FriendActivity extends AppCompatActivity
+{
+
+    Button testButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend);
+
+        testButton = (Button) findViewById(R.id.test_button);
+        testButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(getApplicationContext(), "Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
